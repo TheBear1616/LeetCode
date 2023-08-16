9c081fd4e83c1a29c5633f5f7e6a519940131bb9
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2023-08-16 14:24:02 -0700

    Time: 74 ms (74.53%), Space: 16.8 MB (53.45%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -1,16 +1,22 @@
 class Codec:
-    strDict = {}
     def encode(self, strs: List[str]) -> str:
-        key = ""
+        res = ""
         for word in strs:
-            if len(word) > 0:
-                key += str(len(word)) + word[0]
-        
-        self.strDict[key] = strs 
-        return key
+            res += str(len(word)) + "#" + word
+
+        return res
         
     def decode(self, s: str) -> List[str]:
-        return self.strDict[s]
+        res, i = [], 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            res.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+        return res
         
 
 
