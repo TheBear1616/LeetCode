f0eadacf8c8a5549c9e0f03bf04e32b04fe2e617
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2023-08-19 19:22:23 -0700

    Time: 189 ms (91.09%), Space: 20.8 MB (55.32%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,10 +1,9 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        i = 0
-        j = len(s)-1
-        while i<=j:
-            s[i],s[j] = s[j], s[i]
-            i+=1
-            j-=1
-        return s
+        left, right = 0, len(s)-1
+        
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -= 1
         
