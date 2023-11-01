98488467a9688616604c92025b43f126554b61a8
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2023-10-31 17:13:56 -0700

    Time: 126 ms (13.76%), Space: 17.3 MB (29.71%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -7,10 +7,10 @@ class Solution:
             for c in range(COLS):
                 if matrix[r][c] == 0:
                     matrix[0][c] = 0
-                    if r > 0: 
-                        matrix[r][0] = 0
-                    else:
+                    if r == 0: 
                         rowZero = True
+                    else:
+                        matrix[r][0] = 0
         
         for r in range(1, ROWS):
             for c in range(1, COLS):
