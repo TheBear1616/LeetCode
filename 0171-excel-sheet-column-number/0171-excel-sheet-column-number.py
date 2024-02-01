0a7a64234771cbf40f3ee1e41ea7628d8987a69a
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2024-01-31 19:39:59 -0800

    Time: 42 ms (35.38%), Space: 16.5 MB (72.72%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        result = 0
+
+        for i in range(len(s)):
+            result = result * 26
+            result += (ord(s[i]) - ord("A") + 1)
+
+        return result
