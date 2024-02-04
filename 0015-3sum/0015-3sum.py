4f428d7b36c61e28862d02f981d28dde6cc09911
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2024-02-04 14:26:41 -0800

    Time: 592 ms (86.43%), Space: 20.9 MB (56.57%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,27 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        self.triplets = []
         nums.sort()
+        self.triplets = []
+
         for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i-1]:
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            self.searchTriplet(nums, -nums[i], i+1)
-            
+            self.findTriplets(nums, -nums[i], i+1)
+        
         return self.triplets
-    
-    def searchTriplet(self, nums, target, left):
-        right = len(nums)-1
+            
+    def findTriplets(self, nums, target, left):
+        right = len(nums) - 1
         while left < right:
-            currentSum = nums[left] + nums[right]
-            if target == currentSum:
+            currSum = nums[left] + nums[right]
+            if target == currSum:
                 self.triplets.append([-target, nums[left], nums[right]])
                 left += 1
                 right -= 1
-                while left < right and nums[left] == nums[left-1]:
+                while left < right and nums[left] == nums[left - 1]:
                     left += 1
-                while left < right and nums[right] == nums[right+1]:
-                    right -=1 
-            elif target > currentSum:
-                left += 1
-            else:
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
+            elif target < currSum:
                 right -= 1
+            else:
+                left += 1
