d1fa1faf7f6eb9fb21dcada9a471baba6cef1273
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2024-03-01 17:49:21 -0800

    Time: 53 ms (70.91%), Space: 16.7 MB (83.13%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -3,23 +3,8 @@ class Solution:
         uniqueEmails = set()
 
         for email in emails:
-            cleanMail = []
-
-            for currChar in email:
-                if currChar == '+' or currChar == '@':
-                    break
-
-                if currChar != '.':
-                    cleanMail.append(currChar)
-
-            domainName = []
-            for currChar in reversed(email):
-                domainName.append(currChar)
-                if currChar == '@':
-                    break
-
-            domainName = ''.join(domainName[::-1])
-            cleanMail = ''.join(cleanMail)
-            uniqueEmails.add(cleanMail + domainName)
+            name, domain = email.split('@')
+            local = name.split('+')[0].replace('.', '')
+            uniqueEmails.add(local + '@' + domain)
 
         return len(uniqueEmails)
