ae2d95a6bbd18c491e83a19764c89a9ec67f7cea
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2024-03-05 11:37:16 -0800

    Time: 57 ms (20.38%), Space: 16.6 MB (74.81%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,31 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        que = collections.deque()
-        freshOranges = 0
+        que = collections.deque([])
         time = 0
+        freshOranges = 0
+        directions= [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c] == 1:
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
                     freshOranges += 1
-                if grid[r][c] == 2:
-                    que.append((r, c))
-
-        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        while freshOranges > 0 and que:
-            length = len(que)
-            for i in range(length):
+                elif grid[i][j] == 2:
+                    que.append((i, j))
+        
+        while que and freshOranges > 0:
+            qLEn = len(que)
+            for _ in range(qLEn):
                 r, c = que.popleft()
-
                 for dr, dc in directions:
                     row, col = r + dr, c + dc
-                    if (
-                        row in range(len(grid))
-                        and col in range(len(grid[0]))
-                        and grid[row][col] == 1
-                    ):
+                    if row in range(len(grid)) and col in range(len(grid[0])) and grid[row][col] == 1:
                         grid[row][col] = 2
                         que.append((row, col))
                         freshOranges -= 1
+
             time += 1
+        
         return time if freshOranges == 0 else -1
