60dafcc0b42672ca2a8f26cdcc58ffd7b59b4d7d
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2024-03-15 15:52:43 -0700

    Time: 46 ms (78.43%), Space: 17.2 MB (86.33%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -6,9 +6,9 @@ class Solution:
             mid = (low + high) // 2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                low = mid + 1
-            else:
+            elif nums[mid] > target:
                 high = mid - 1
-
+            else:
+                low = mid + 1
+        
         return low
