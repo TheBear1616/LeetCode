e095ab541be14e497c65211624f17f8f7577c7a7
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2024-03-20 21:40:15 -0700

    Time: 357 ms (82.71%), Space: 30.8 MB (28.20%) - LeetHub

--- 2340-minimum-adjacent-swaps-to-make-a-valid-array/2340-minimum-adjacent-swaps-to-make-a-valid-array.py
+++ 2340-minimum-adjacent-swaps-to-make-a-valid-array/2340-minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -4,24 +4,15 @@ class Solution:
         maxEleIdx = -1
         minEle = 1e9
         minEleIdx = -1
-        swaps = 0
 
         for i in range(len(nums)):
             if nums[i] >= maxEle:
                 maxEle = nums[i]
                 maxEleIdx = i
-
-        for i in range(maxEleIdx, len(nums) - 1):
-            nums[i], nums[i+1] = nums[i+1], nums[i]
-            swaps += 1
-        
-        for i in range(len(nums)-1, -1, -1):
-            if nums[i] <= minEle:
+            if nums[i] < minEle:
                 minEle = nums[i]
                 minEleIdx = i
-        
-        for i in range(minEleIdx, 0, -1):
-            nums[i], nums[i-1] = nums[i-1], nums[i]
-            swaps += 1
 
-        return swaps
+        swaps = minEleIdx + (len(nums) - 1 - maxEleIdx)
+
+        return swaps - 1 if minEleIdx > maxEleIdx else swaps
