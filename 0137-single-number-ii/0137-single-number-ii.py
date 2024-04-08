edc718e587234c91635d0e369a083373afdf7f86
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2024-04-08 16:39:14 -0700

    Time: 43 ms (98.84%), Space: 18.6 MB (82.63%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen_once = seen_twice = 0
+
+        for num in nums:
+            seen_once = (seen_once ^ num) & (~seen_twice)
+            seen_twice = (seen_twice ^ num) & (~seen_once)
+
+        return seen_once
