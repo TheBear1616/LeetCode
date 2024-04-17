5adf6781cd684395e9f250a20cb920d81e820804
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2024-04-16 20:15:53 -0700

    Time: 98 ms (26.26%), Space: 16.5 MB (95.43%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,21 +1,21 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)  # key = (r /3, c /3)
-        
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == ".":
-                    continue
-                if (
-                    board[r][c] in rows[r]
-                    or board[r][c] in cols[c]
-                    or board[r][c] in squares[(r // 3, c // 3)]
-                ):
-                    return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
+        row = collections.defaultdict(set)
+        col = collections.defaultdict(set)
+        sqr = collections.defaultdict(set)
 
+        for i in range(9):
+            for j in range(9):
+                value = board[i][j]
+
+                if value == ".":
+                    continue
+                    
+                if value in row[i] or value in col[j] or value in sqr[(i // 3, j // 3)]:
+                    return False
+                
+                row[i].add(value)
+                col[j].add(value)
+                sqr[(i // 3, j // 3)].add(value)
+        
         return True
