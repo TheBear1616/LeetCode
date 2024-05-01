0f964a6c9d0c7821954b3036e1f1fa771d7d5d00
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2024-05-01 15:38:24 -0700

    Time: 44 ms (5.40%), Space: 16.5 MB (45.57%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -3,10 +3,10 @@ class Solution:
         ranges = []     
         i = 0 
         
-        while i < len(nums): 
-            start = nums[i]  
+        while i < len(nums):
+            start = nums[i]
             while i + 1 < len(nums) and nums[i] + 1 == nums[i + 1]: 
-                i += 1 
+                i += 1
             
             if start != nums[i]: 
                 ranges.append(str(start) + "->" + str(nums[i]))
