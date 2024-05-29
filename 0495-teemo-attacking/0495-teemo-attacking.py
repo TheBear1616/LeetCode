9af03dd91eedd31f8d0fcd06802ca5b3c82e7e27
Mayur Shivakumar <118773218+TheBear1616@users.noreply.github.com>
2024-05-28 18:56:32 -0700

    Time: 208 ms (12.96%), Space: 18.5 MB (5.47%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.py
+++ 0495-teemo-attacking/0495-teemo-attacking.py
@@ -1,9 +1,15 @@
 class Solution:
     def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
-        n = len(timeSeries)        
-        total = 0
+        result = [[timeSeries[0], timeSeries[0] + duration - 1]]
+        ans = 0
 
-        for i in range(n - 1):
-            total += min(timeSeries[i + 1] - timeSeries[i], duration)
-            
-        return total + duration
+        for time in timeSeries[1:]:
+            if result[-1][-1] >= time:
+                result[-1][-1] = max(result[-1][-1], time + duration - 1)
+            else:
+                result.append([time, time + duration - 1])
+        
+        for [start, end] in result:
+            ans += end - start + 1
+
+        return ans
